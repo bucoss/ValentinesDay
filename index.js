fcc232059da05a255dfdf7622996c10d29d99f40
bucoss <cosmin.burechita@gmail.com>
2025-02-05 15:53:09 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -2,36 +2,36 @@
 const yesButton = document.querySelector(".yesButton");
 const noButton = document.querySelector(".noButton");
 
-// Function to increase the size of the "Yes" button by 5%
 function increaseYesButtonSize() {
-  // Get the current width and height of the "Yes" button
   const currentWidth = parseFloat(window.getComputedStyle(yesButton).width);
   const currentHeight = parseFloat(window.getComputedStyle(yesButton).height);
 
-  // Calculate the new size (5% bigger)
   const newWidth = currentWidth * 1.25;
   const newHeight = currentHeight * 1.25;
 
-  // Apply the new size to the "Yes" button
   yesButton.style.width = `${newWidth}px`;
   yesButton.style.height = `${newHeight}px`;
+
+  const currentWidthNo = parseFloat(window.getComputedStyle(noButton).width);
+  const currentHeightNo = parseFloat(window.getComputedStyle(noButton).height);
+
+  const newWidthNo = currentWidthNo / 1.25;
+  const newHeightNo = currentHeightNo / 1.25;
+
+  noButton.style.width = `${newWidthNo}px`;
+  noButton.style.height = `${newHeightNo}px`;
 }
 
-// Add an event listener to the "No" button
 noButton.addEventListener("click", increaseYesButtonSize);
 
 const image = document.querySelector(".poza-titlu img");
 
-// Function to handle the "Yes" button click
 function handleYesButtonClick() {
-  // Hide the buttons
   yesButton.style.display = "none";
   noButton.style.display = "none";
   document.querySelector(".willYou").innerHTML = "Thanks Bibi! Love you!";
 
-  // Change the image source
-  image.src = "./media/tomKiss.jpg"; // Replace with the path to your new image
+  image.src = "./media/tomKiss.jpg";
 }
 
-// Add an event listener to the "Yes" button
 yesButton.addEventListener("click", handleYesButtonClick);
